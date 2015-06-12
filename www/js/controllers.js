bbc482eb7d6ed527d5ef7f018200a49b5e76683a
Zeuxis Lo <seekstudio@gmail.com>
2015-06-12 14:46:13 +0800

    support pull to refresh function to load latest images

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -18,15 +18,37 @@ app.controller('HomeCtrl', function(app, $scope, $location) {
 
             app.toast.error('Can not load the images');
         });
-
     }
 
     $scope.images       = [];
     $scope.hasMore      = false;
     $scope.page         = 1;
     $scope.loadingMore  = false;
-    $scope.loadMore    = function() {
+
+    $scope.loadMore = function() {
         loadPage();
+    };
+
+    $scope.doRefresh = function() {
+        $scope.loadingMore = true;
+
+        app.restAPI.api.get({
+            'token': app.apiInfo.token,
+            'page' : 1
+        }, function(http) {
+            var images = http.items;
+
+            $scope.images      = images.concat($scope.images);
+            $scope.loadingMore = false;
+
+            $scope.$broadcast('scroll.refreshComplete');
+        }, function(http) {
+            $scope.loadingMore = false;
+
+            $scope.$broadcast('scroll.refreshComplete');
+
+            app.toast.error('Can not load the latest images');
+        });
     };
 
     loadPage();
